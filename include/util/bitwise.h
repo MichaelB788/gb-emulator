592e0fd48a321fe8847578aa29130f7a136e6ad8
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-04-14 15:07:14 -0400

    Using boolean parameter for easy setting

--- include/util/bitwise.h
+++ include/util/bitwise.h
@@ -1,14 +1,14 @@
 #pragma once
+#include <stdbool.h>
 #include <stdint.h>
 
-inline void set_bit(uint8_t *byte, uint8_t bit_index) {
-  *byte |= (uint8_t)(1 << bit_index);
+static inline void set_bit(uint8_t *byte, uint8_t bit_index, bool value) {
+  if (value)
+    *byte |= (uint8_t)(1 << bit_index);
+  else
+    *byte &= (uint8_t)~(1 << bit_index);
 }
 
-inline void clear_bit(uint8_t *byte, uint8_t bit_index) {
-  *byte &= (uint8_t)(~(1 << bit_index));
-}
-
-inline uint8_t get_bit(uint8_t byte, uint8_t bit_index) {
+static inline uint8_t get_bit(uint8_t byte, uint8_t bit_index) {
   return (byte >> bit_index) & 1;
 }
