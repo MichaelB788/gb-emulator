7eebe71d8684007957e4f34cf85d48cf0d2ee669
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-05-03 22:32:10 +0000

    Moved definitions to source file, changed internal_rom_addr parameter ordering

--- include/core/mapper.h
+++ include/core/mapper.h
@@ -2,13 +2,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-static inline uint16_t internal_rom_addr(uint8_t rom_bank, uint16_t addr) {
-  return addr + (rom_bank * 0x4000);
-}
+uint16_t internal_rom_addr(uint16_t addr, uint8_t rom_bank);
 
-static inline uint16_t internal_ram_addr(uint16_t addr, uint8_t ram_bank,
-                                         size_t max_external_ram_size) {
-  // 0xA000 is the start address of exRAM, and this section is 0x2000 (8192)
-  // bytes long.
-  return ((addr - 0xA000) + (ram_bank * 0x2000)) % max_external_ram_size;
-}
+uint16_t internal_ram_addr(uint16_t addr, uint8_t ram_bank,
+                           size_t max_external_ram_size);
--- src/core/mapper.c
+++ src/core/mapper.c
@@ -1,3 +1,12 @@
 #include "core/mapper.h"
-#include <stddef.h>
-#include <stdint.h>
+
+uint16_t internal_rom_addr(uint16_t addr, uint8_t rom_bank) {
+  return addr + (rom_bank * 0x4000);
+}
+
+uint16_t internal_ram_addr(uint16_t addr, uint8_t ram_bank,
+                           size_t max_external_ram_size) {
+  // 0xA000 is the start address of exRAM, and this section is 0x2000 (8192)
+  // bytes long.
+  return ((addr - 0xA000) + (ram_bank * 0x2000)) % max_external_ram_size;
+}
