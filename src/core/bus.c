6d2955127a25ee7332522879ed35a17ada94d06c
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-05-08 17:31:32 -0400

    Created an enum for easier parsing of memory map

--- src/core/bus.c
+++ src/core/bus.c
@@ -2,48 +2,90 @@
 #include "core/cartridge.h"
 #include <stdint.h>
 
+enum {
+  ROM_END = 0x8000,
+  VRAM_END = 0xA000,
+  EXRAM_END = 0xC000,
+  WRAM_BEGIN = 0xC000,
+  WRAM_END = 0xE000,
+  ECHO_BEGIN = 0xFE00,
+  ECHO_END = 0xFE00,
+  OAM_END = 0xFEA0,
+  PROHIBITED_END = 0xFF00,
+  IO_END = 0xFF80,
+  HRAM_BEGIN = 0xFF80,
+  HRAM_END = 0xFFFE
+};
+
 uint8_t read_byte(Bus *bus, uint16_t addr) {
-  if (addr < 0x8000) // ROM
+  if (addr < ROM_END) {
     return read_rom(bus->cartridge, addr);
-  if (addr < 0xA000) // TODO: VRAM
+  }
+  if (addr < VRAM_END) {
+    // TODO: VRAM
     return 0x10;
-  if (addr < 0xC000) // External RAM
+  }
+  if (addr < EXRAM_END) {
     return read_ram(bus->cartridge, addr);
-  if (addr < 0xE000) // Work RAM (No banking)
-    return bus->wram[addr - 0xC000];
-  if (addr < 0xFE00) // Echo RAM
-    return bus->wram[addr - 0xE000];
-  if (addr < 0xFEA0) // TODO: OAM
+  }
+  if (addr < WRAM_END) {
+    return bus->wram[addr - WRAM_BEGIN];
+  }
+  if (addr < ECHO_END) {
+    return bus->wram[addr - ECHO_BEGIN];
+  }
+  if (addr < OAM_END) {
+    // TODO: OAM
     return 0x10;
-  if (addr < 0xFF00) // Prohibited space
+  }
+  if (addr < PROHIBITED_END) {
     return 0x10;
-  if (addr < 0xFF80) // TODO: IO Registers
+  }
+  if (addr < IO_END) {
+    // TODO: IO Registers
     return 0x10;
-  if (addr < 0xFFFE) // High RAM
-    return bus->hram[addr - 0xFF80];
-  else // Interrupt Enable Register
-    return bus->interrupt_enable;
+  }
+  if (addr < HRAM_END) {
+    return bus->hram[addr - HRAM_BEGIN];
+  }
+  return bus->interrupt_enable;
 }
 
 void write_byte(Bus *bus, uint16_t addr, uint8_t val) {
-  if (addr < 0x8000) // ROM
+  if (addr < ROM_END) {
     write_rom(bus->cartridge, addr, val);
-  else if (addr < 0xA000) // TODO: VRAM
     return;
-  else if (addr < 0xC000) // External RAM
+  }
+  if (addr < VRAM_END) {
+    // TODO: VRAM
+    return;
+  }
+  if (addr < EXRAM_END) {
     write_ram(bus->cartridge, addr, val);
-  else if (addr < 0xE000) // Work RAM (No banking)
-    bus->wram[addr - 0xC000] = val;
-  else if (addr < 0xFE00) // Echo RAM
-    bus->wram[addr - 0xE000] = val;
-  else if (addr < 0xFEA0) // TODO: OAM
     return;
-  else if (addr < 0xFF00) // Prohibited space
+  }
+  if (addr < WRAM_END) {
+    bus->wram[addr - WRAM_BEGIN] = val;
     return;
-  else if (addr < 0xFF80) // TODO: IO Registers
+  }
+  if (addr < ECHO_END) {
+    bus->wram[addr - ECHO_BEGIN] = val;
     return;
-  else if (addr < 0xFFFE) // High RAM
-    bus->hram[addr - 0xFF80] = val;
-  else // Interrupt Enable Register
-    bus->interrupt_enable = val & 1;
+  }
+  if (addr < OAM_END) {
+    // TODO: OAM
+    return;
+  }
+  if (addr < PROHIBITED_END) {
+    return;
+  }
+  if (addr < IO_END) {
+    // TODO: IO Registers
+    return;
+  }
+  if (addr < HRAM_END) {
+    bus->hram[addr - HRAM_BEGIN] = val;
+    return;
+  }
+  bus->interrupt_enable = val;
 }
