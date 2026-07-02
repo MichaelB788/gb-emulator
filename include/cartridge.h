d6a993385531217065affa3f08cf04cfb0036b70
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-07-02 15:11:45 -0400

    Updated Cartridge code

--- include/cartridge.h
+++ include/cartridge.h
@@ -4,13 +4,17 @@
 #include <stddef.h>
 #include <stdint.h>
 
-enum mapper {
-  ROM_MAPPER,
-  MBC1_MAPPER,
+enum cartridge_type {
+  ROM_ONLY_CART = 0x00,
+  MBC1_CART = 0x01,
+  MBC1_RAM_CART = 0x02,
+  MBC1_RAM_BATTERY_CART = 0x03,
+  MBC2_CART = 0x05,
+  MBC2_BATTERY_CART = 0x06,
 };
 
 struct cartridge {
-  enum mapper mapper;
+  enum cartridge_type type;
   union {
     struct mbc1 mbc1;
   };
@@ -26,10 +30,10 @@ bool init_cartridge(struct cartridge *cart, const char *path_to_rom);
 
 void destroy_cartridge(struct cartridge *cart);
 
-uint8_t mapper_read(const struct cartridge *cart, uint16_t addr);
+uint8_t rom_read(const struct cartridge *cart, uint16_t addr);
 
-void mapper_write(struct cartridge *cart, uint16_t addr, uint8_t val);
+void rom_write(struct cartridge *cart, uint16_t addr, uint8_t val);
 
-uint8_t exram_read(struct cartridge *cart, uint16_t addr);
+uint8_t ram_read(struct cartridge *cart, uint16_t addr);
 
-void exram_write(struct cartridge *cart, uint16_t addr, uint8_t val);
+void ram_write(struct cartridge *cart, uint16_t addr, uint8_t val);
--- src/cartridge.c
+++ src/cartridge.c
@@ -7,39 +7,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void init_mapper(struct cartridge *cart) {
-  switch (cart->mapper) {
-  case ROM_MAPPER:
+bool init_mapper(struct cartridge *cart) {
+  switch (cart->type) {
+  case ROM_ONLY_CART:
     break;
-
-  case MBC1_MAPPER:
+  case MBC1_CART:
     init_mbc1(&cart->mbc1);
     break;
-  }
-}
-
-bool read_header(FILE *rom_file, struct cartridge *cart) {
-  uint8_t header[0x150] = {0};
-  if (fread(header, 1, 0x150, rom_file) != 0x150) {
-    perror("Could not read file content into header");
+  default:
+    fprintf(stderr, "Cannot init unknown mapper %d", (int)cart->type);
     return false;
   }
-
-  /**
-   * Note: A RAM size byte with value 0x1 isn't used by any official ROMS, so
-   * the actual size is unknown. I just assume no RAM exists.
-   */
-  const size_t ram_sizes[6] = {0, 0, KiB_8, KiB_32, KiB_128, KiB_64};
-  cart->mapper = header[0x147];
-  cart->rom_size = KiB_32 * (1 << header[0x148]);
-  cart->ram_size = ram_sizes[header[0x149]];
-  init_mapper(cart);
-
   return true;
 }
 
 bool initialize_rom(FILE *rom_file, struct cartridge *cart) {
-  rewind(rom_file);
   cart->rom = malloc(cart->rom_size);
   if (!cart->rom) {
     perror("Failed to malloc ROM");
@@ -63,15 +45,33 @@ bool initialize_ram(struct cartridge *cart) {
   return true;
 }
 
+bool initialize_cart_from_header(FILE *rom_file, struct cartridge *cart) {
+  uint8_t header[0x150] = {0};
+  if (fread(header, 1, 0x150, rom_file) != 0x150) {
+    perror("Could not read file content into header");
+    return false;
+  }
+  rewind(rom_file);
+
+  cart->type = (enum cartridge_type)header[0x147];
+  cart->rom_size = KiB_32 * (1 << header[0x148]);
+
+  // ram_sizes[0x01] is unused by any official ROMs, so it's unknown what RAM
+  // size it should correspond to, if any.
+  const size_t ram_sizes[] = {0, 0, KiB_8, KiB_32, KiB_128, KiB_64};
+  cart->ram_size = ram_sizes[header[0x149]];
+
+  return init_mapper(cart) && initialize_rom(rom_file, cart) &&
+         initialize_ram(cart);
+}
+
 bool init_cartridge(struct cartridge *cart, const char *path_to_rom) {
   FILE *rom_file = fopen(path_to_rom, "rb");
   if (!rom_file) {
     perror("Could not open file");
     return false;
   }
-
-  if (read_header(rom_file, cart) && initialize_rom(rom_file, cart) &&
-      initialize_ram(cart)) {
+  if (initialize_cart_from_header(rom_file, cart)) {
     fclose(rom_file);
     return true;
   } else {
@@ -88,56 +88,55 @@ void destroy_cartridge(struct cartridge *cart) {
   }
 }
 
-uint8_t mapper_read(const struct cartridge *cart, const uint16_t addr) {
-  switch (cart->mapper) {
-  case ROM_MAPPER:
+uint8_t rom_read(const struct cartridge *cart, const uint16_t addr) {
+  switch (cart->type) {
+  case ROM_ONLY_CART:
     return cart->rom[addr];
-
-  case MBC1_MAPPER:
+  case MBC1_CART:
     return mbc1_read_rom(cart, addr);
+  default:
+    fprintf(stderr, "Warn: Unknown mapper type, cannot read ROM");
+    return 0xFF;
   }
 }
 
-void mapper_write(struct cartridge *cart, const uint16_t addr,
-                  const uint8_t val) {
-  switch (cart->mapper) {
-  case ROM_MAPPER:
-    // ROM is read only, so this is a no-op.
+void rom_write(struct cartridge *cart, const uint16_t addr, const uint8_t val) {
+  switch (cart->type) {
+  case ROM_ONLY_CART:
     break;
-
-  case MBC1_MAPPER:
+  case MBC1_CART:
     mbc1_write_rom(cart, addr, val);
     break;
+  default:
+    fprintf(stderr, "Warn: Unknown mapper type, cannot write ROM");
+    break;
   }
 }
 
-uint8_t exram_read(struct cartridge *cart, const uint16_t addr) {
+uint8_t ram_read(struct cartridge *cart, const uint16_t addr) {
   if (cart->ram_size > 0) {
-    switch (cart->mapper) {
-    case ROM_MAPPER:
-      // No RAM available
-      return 0xFF;
-
-    // MBC1 mappers with built in RAM
-    case MBC1_MAPPER:
+    switch (cart->type) {
+    case MBC1_RAM_CART:
+    case MBC1_RAM_BATTERY_CART:
       return mbc1_read_ram(cart, addr);
+    default:
+      break;
     }
   }
+  fprintf(stderr, "Warn: Attempt to read RAM that doesn't exist");
   return 0xFF;
 }
 
-void exram_write(struct cartridge *cart, const uint16_t addr,
-                 const uint8_t val) {
+void ram_write(struct cartridge *cart, const uint16_t addr, const uint8_t val) {
   if (cart->ram_size > 0) {
-    switch (cart->mapper) {
-    // Mappers with no ram.
-    case ROM_MAPPER:
-      break;
-
-    // MBC1 mappers with RAM
-    case MBC1_MAPPER:
+    switch (cart->type) {
+    case MBC1_RAM_CART:
+    case MBC1_RAM_BATTERY_CART:
       mbc1_write_ram(cart, addr, val);
+      break;
+    default:
       break;
     }
   }
+  fprintf(stderr, "Warn: Attempt to write RAM that doesn't exist");
 }
