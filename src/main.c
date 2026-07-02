df82c7994da92a622b7bc68e04d92072be95377c
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-07-02 16:04:05 -0400

    Moving main loop inside of a dedicated function

--- src/main.c
+++ src/main.c
@@ -1,18 +1,5 @@
 #include "gameboy.h"
-#include "optables.h"
 #include <stdint.h>
-#include <stdio.h>
-
-void log_instruction(struct gameboy *gb, FILE *output) {
-  const struct cpu cpu = gb->cpu;
-  fprintf(output,
-          "A:%02X F:%02X B:%02X C:%02X D:%02X E:%02X H:%02X L:%02X SP:%04X "
-          "PC:%04X PCMEM:%02X,%02X,%02X,%02X\n",
-          cpu.A, cpu.F, cpu.B, cpu.C, cpu.D, cpu.E, cpu.H, cpu.L, cpu.SP,
-          cpu.PC, bus_read(gb, cpu.PC), bus_read(gb, cpu.PC + 1),
-          bus_read(gb, cpu.PC + 2), bus_read(gb, cpu.PC + 3));
-  fflush(output);
-}
 
 int main(const int argc, const char **argv) {
   if (argc < 2) {
@@ -20,24 +7,11 @@ int main(const int argc, const char **argv) {
     return 1;
   }
 
-  FILE *log_file = fopen("log.txt", "w");
-  if (!log_file) {
-    perror("Couldn't open log.txt");
-    return 1;
-  }
-
   struct gameboy gb = {0};
   if (init_gameboy(&gb, argv[1])) {
-    while (gb.state == GB_RUNNING) {
-      gb.opcode = bus_read(&gb, gb.cpu.PC);
-      log_instruction(&gb, log_file);
-      ++gb.cpu.PC;
-      int cycles = unprefixed_ins[gb.opcode](&gb);
-    }
+    run_gameboy_loop(&gb);
     close_gameboy(&gb);
   } else {
     fprintf(stderr, "Could not initialize GameBoy.\n");
   }
-
-  fclose(log_file);
 }
