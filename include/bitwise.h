7de4a6cae5597a6ae369dcf8b5fc85899dc03aff
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-07-03 11:57:17 -0400

    Added function for toggling a bit

--- include/bitwise.h
+++ include/bitwise.h
@@ -2,15 +2,17 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-static void set_bit(uint8_t *target, uint8_t i) { *target |= (1 << i); }
+static void set_bit(uint8_t *target, uint8_t i) { *target |= 1 << i; }
 
 static void clear_bit(uint8_t *target, uint8_t i) { *target &= ~(1 << i); }
 
-static bool is_bit_set(uint8_t target, uint8_t i) { return (target >> i) & 1; }
+static void toggle_bit(uint8_t *target, uint8_t i) { *target ^= 1 << i; }
+
+static bool is_bit_set(uint8_t target, uint8_t i) { return target >> i & 1; }
 
 static void set_bit_as(uint8_t *target, uint8_t i, bool val) {
   if (val) {
-    *target |= (1 << i);
+    *target |= 1 << i;
   } else {
     *target &= ~(1 << i);
   }
