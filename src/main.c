ae0e7e242d6355ceb1e08e053fdc7baec0194108
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-07-06 09:46:55 -0400

    Touched up on main function

--- src/main.c
+++ src/main.c
@@ -10,10 +10,7 @@
 #include <SDL3/SDL_main.h>
 
 static struct gameboy gb = {0};
-
-#ifndef NDEBUG
 static FILE *log_file = NULL;
-#endif
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
   if (argc < 2) {
@@ -31,37 +28,29 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
     return SDL_APP_FAILURE;
   }
 
-#ifndef NDEBUG
   if (argv[2] && strcmp(argv[2], "--logging_enabled") == 0) {
     log_file = fopen("log.txt", "w");
     if (!log_file) {
       perror("Could not open log file");
     }
   }
-#endif
 
   return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate) {
   int cycles = 0;
-
-  // Check to see if interrupts were enabled
   gb.cpu.IME = gb.cpu.enable_interrupts;
 
-  // Execute the next CPU instruction
   gb.opcode = bus_read(&gb, gb.cpu.PC);
-#ifndef NDEBUG
-  if (log_file)
+  if (log_file) {
     log_curr_instr(&gb, log_file);
-#endif
+  }
   ++gb.cpu.PC;
-  cycles += unprefixed_ins[gb.opcode](&gb);
 
-  // Handle interrupts
+  cycles += unprefixed_ins[gb.opcode](&gb);
   cycles += handle_interrupts(&gb);
 
-  // Ticks
   timer_tick(&gb.timer, cycles, &gb.interrupt);
 
   return SDL_APP_CONTINUE;
@@ -81,10 +70,13 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
   close_gameboy(&gb);
 
-#ifndef NDEBUG
-  if (log_file)
+  if (log_file) {
     fclose(log_file);
-#endif
+  }
 
-  printf("Program exited with: %d\n", result);
+  if (result == SDL_APP_SUCCESS) {
+    printf("Program exited successfully\n");
+  } else {
+    printf("Program exited unsuccessfully with: %d\n", result);
+  }
 }
