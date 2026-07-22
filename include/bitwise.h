a854f6c1af3e1215ca08032da55ef1430d2d65bf
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-07-22 00:48:51 -0400

    Changed name of set_bit_as

--- include/bitwise.h
+++ include/bitwise.h
@@ -3,14 +3,12 @@
 #include <stdint.h>
 
 static void set_bit(uint8_t *target, uint8_t i) { *target |= 1 << i; }
-
 static void clear_bit(uint8_t *target, uint8_t i) { *target &= ~(1 << i); }
-
 static void toggle_bit(uint8_t *target, uint8_t i) { *target ^= 1 << i; }
 
 static bool is_bit_set(uint8_t target, uint8_t i) { return target >> i & 1; }
 
-static void set_bit_as(uint8_t *target, uint8_t i, bool val) {
+static void write_bit(uint8_t *target, uint8_t i, bool val) {
   if (val) {
     *target |= 1 << i;
   } else {
