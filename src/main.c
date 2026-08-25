7e84a1d469048e991a3af2cf07c476b02fbe8b6f
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-08-24 20:07:22 -0400

    Using C23 and removed compile.sh script

--- src/main.c
+++ src/main.c
@@ -35,6 +35,7 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 }
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+  (void)appstate;
   switch (event->type) {
   case SDL_EVENT_QUIT:
     return SDL_APP_SUCCESS;
