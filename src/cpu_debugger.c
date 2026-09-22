d322940d80d3b6228caa36ae4417669d5d772b12
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-09-22 16:48:31 -0400

    Parsing user input within cpu_debugger_interactive_menu
    Changed scanned input to hexadecimal

--- src/cpu_debugger.c
+++ src/cpu_debugger.c
@@ -12,7 +12,7 @@
 static void push_unique_address_from_input(struct u16_stk *out) {
   uint16_t address;
   printf("Address: ");
-  scanf("%hu", &address);
+  scanf("%hx", &address);
   if (!u16_stk_contains(out, address)) {
     if (!u16_stk_push(out, address))
       fprintf(stderr, "Stack at max capacity. Value discarded.\n");
@@ -45,63 +45,60 @@ enum dbg_flags {
   // clang-format off
   DBG_WATCHPOINT = 1 << 0,
   DBG_BREAKPOINT = 1 << 1,
-  DBG_STEP       = 1 << 2,
+  DBG_STEP       = 1 << 2
   // clang-format on
 };
-static void cpu_debugger_parse_input(struct cpu_debugger *dbg, struct cpu *cpu,
-                                     char input, enum dbg_flags flags) {
+
+static void cpu_debugger_interactive_menu(struct cpu_debugger *dbg,
+                                          struct cpu *cpu,
+                                          enum dbg_flags flags) {
+  char input;
+  printf("[%s%s%s(c)ontinue]: ", flags & DBG_STEP ? "(s)tep | " : "",
+         flags & DBG_BREAKPOINT ? "(b)reakpoint | " : "",
+         flags & DBG_WATCHPOINT ? "(w)atchpoint | " : "");
+  scanf(" %c", &input);
   switch (input) {
   case 's':
-    if (flags & DBG_STEP) {
+    if (flags & DBG_STEP)
       cpu_step(cpu);
-      return;
-    }
+    else
+      puts("Invalid option");
     break;
   case 'b':
-    if (flags & DBG_BREAKPOINT) {
+    if (flags & DBG_BREAKPOINT)
       push_unique_address_from_input(&dbg->breakpoints);
-      return;
-    }
+    else
+      puts("Invalid option");
     break;
   case 'w':
-    if (flags & DBG_WATCHPOINT) {
+    if (flags & DBG_WATCHPOINT)
       push_unique_address_from_input(&dbg->watchpoints);
-      return;
-    }
+    else
+      puts("Invalid option");
     break;
   case 'c':
     dbg->state = CPU_DEBUGGER_IDLE;
     cpu->log_level = CPU_LOG_NONE;
-    return;
+    break;
   }
-  puts("Invalid option.");
 }
 
 void cpu_debugger_step(struct cpu_debugger *dbg, struct cpu *cpu) {
   switch (dbg->state) {
   case CPU_DEBUGGER_INIT:
-    char user_input;
-    printf("\n[(b)reakpoint | (w)atchpoint | (c)ontinue]: ");
-    scanf(" %c", &user_input);
-    putchar('\n');
-    cpu_debugger_parse_input(dbg, cpu, user_input,
-                             DBG_BREAKPOINT | DBG_WATCHPOINT);
+    cpu_debugger_interactive_menu(dbg, cpu, DBG_BREAKPOINT | DBG_WATCHPOINT);
     break;
-  case CPU_DEBUGGER_INTERACTIVE: {
-    char user_input;
-    printf("\n[(s)tep | (b)reakpoint | (w)atchpoint | (c)ontinue]: ");
-    scanf(" %c", &user_input);
-    putchar('\n');
-    cpu_debugger_parse_input(dbg, cpu, user_input,
-                             DBG_STEP | DBG_BREAKPOINT | DBG_WATCHPOINT);
-  } break;
-  case CPU_DEBUGGER_IDLE: {
+  case CPU_DEBUGGER_INTERACTIVE:
+    cpu_debugger_interactive_menu(dbg, cpu,
+                                  DBG_STEP | DBG_BREAKPOINT | DBG_WATCHPOINT);
+    break;
+  case CPU_DEBUGGER_IDLE:
     if (u16_stk_contains(&dbg->breakpoints, cpu->PC)) {
       printf("Breakpoint 0x%04X hit.\n", cpu->PC);
       dbg->state = CPU_DEBUGGER_INTERACTIVE;
       cpu->log_level = CPU_LOG_VERBOSE;
     }
     cpu_step(cpu);
-  } break;
+    break;
   }
 }
