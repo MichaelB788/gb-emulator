8c8a590ae69ffc42b11c99299879c22704cc52d9
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-09-23 10:04:32 -0400

    script searches and produces output within the scripts/ directory
    script generates a pragma once header guard

--- scripts/generate_mnemonics.py
+++ scripts/generate_mnemonics.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 import json
+import os.path
 
 # Script uses data from https://github.com/izik1/gbops/blob/master/dmgops.json
 # Assumes dmgops.json is in the same directory as this script
 
-with open("dmgops.json", "r") as dmgops_json:
+file_dir = os.path.dirname(os.path.realpath(__file__))
+
+with open(os.path.join(file_dir, "dmgops.json"), "r") as dmgops_json:
     data = json.load(dmgops_json)
     names_of = lambda prefix: ",\n".join(
         f'  "{entry["Name"]}"' for entry in data[prefix]
     )
-    with open("mnemonics.h", "w") as output:
-        output.write(
+    with open(os.path.join(file_dir, "mnemonics.h"), "w") as mnemonics_h:
+        mnemonics_h.write(
             "\n".join(
                 [
+                    "#pragma once",
+                    "",
                     "static const char *mnemonics[256] = {",
                     names_of("Unprefixed"),
                     "};",
